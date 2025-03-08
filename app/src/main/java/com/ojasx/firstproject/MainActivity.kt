d450f4268d6abe42a147ac0dbda917dd7eec39c3
ojas <ojas1430@gmail.com>
2025-03-08 20:17:26 +0530

    login ui created

--- app/src/main/java/com/ojasx/firstproject/MainActivity.kt
+++ app/src/main/java/com/ojasx/firstproject/MainActivity.kt
@@ -1,78 +1,218 @@
+@file:Suppress("UNREACHABLE_CODE")
+
 package com.ojasx.firstproject
 
+import android.annotation.SuppressLint
+import android.graphics.Paint.Style
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.elevatedCardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Label
+import androidx.compose.material3.LeadingIconTab
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextMotion
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ojasx.firstproject.ui.theme.FirstProjectTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import java.nio.file.WatchEvent
 
+
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FirstProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+             timepass()
         }
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    layout()
-
-    }
 @Preview
 @Composable
-fun layout(){
-    Box(modifier = Modifier.fillMaxSize().
-    background(color = Color.Black),
-        contentAlignment = Alignment.Center,
-        
-    ){
-        Text("Ojas is a good boy",
-            style = TextStyle(Color.Blue)
-            , fontSize = 69.sp
-            , fontWeight = FontWeight(550)
-            ,
-            )
-        Text("Ojas is a good boy"
-            ,modifier = Modifier.background(color = Color.Magenta),
-            fontWeight= FontWeight(40))
-    }
+fun timepass(){
+    
+    var name by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
 
+    var context = LocalContext.current
+
+    
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+
+        Text("Welcome to Our Site",
+            style = MaterialTheme.typography.headlineLarge)
+
+        Spacer(Modifier.height(16.dp))
+
+       // Name
+        OutlinedTextField(value = name,
+            onValueChange = {name = it},
+            label = {
+                Text("Name")
+            },
+            placeholder = {
+                Text("Enter your name")
+            }, leadingIcon = {
+                Icon(Icons.Default.Person ,
+                    contentDescription = "")
+            }, singleLine = true
+            , keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text )
+            , textStyle = TextStyle(fontSize = 18.sp)
+        )
+
+        // Email
+        OutlinedTextField(value = email,
+            onValueChange = {email = it}
+            , label = {
+                Text("Email")
+            },
+            placeholder = {
+                Text("Enter your Email")
+            }
+            , leadingIcon = {
+                Icon(imageVector = Icons.Default.Email,
+                    contentDescription = "")
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email
+            ), textStyle = TextStyle(fontSize = 18.sp)
+        )
+
+        // password
+        OutlinedTextField(value = password,
+            onValueChange = {password = it}
+            , label = {
+                Text("Password")
+            },
+            placeholder = {
+                Text("Enter your Password")
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Lock,
+                    contentDescription = "")
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            visualTransformation = PasswordVisualTransformation(),
+            textStyle = TextStyle(fontSize = 18.sp)
+        )
+
+        // Login Button
+        Spacer(Modifier.height(16.dp))
+        Button(onClick = {
+            Toast.makeText(context ,"Welcome $name",
+                Toast.LENGTH_LONG).show()
+        },
+            modifier = Modifier.fillMaxWidth()) {
+            Text("Login")
+        }
+
+
+    }
 }
+
+
